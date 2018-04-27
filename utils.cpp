4351ce17efa7514816e6705e6da3cf09bedbb121
Malfoy <antoine.limasset@gmail.com>
2018-04-27 10:42:52 +0200

    added codemiss

--- utils.cpp
+++ utils.cpp
@@ -184,9 +184,11 @@ string codeMiss(const string& read, const string& ref){
 	string res;
 	for(uint i(0);i<read.size();++i){
 		if(read[i]!=ref[i]){
-			//~ res+=(char)i;
-			res+=to_string(i);
-			res+=to_string(int(nuc2int(read[i])-nuc2int(ref[i])));
+			res+=(char)i;
+			//~ res+=to_string(i);
+			int diff_nuc(nuc2int(read[i])-nuc2int(ref[i]));
+			if(diff_nuc<0){diff_nuc+=4;}
+			res+=(char)diff_nuc;
 		}
 	}
 	return res;
